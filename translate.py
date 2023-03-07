0fbc9cbc6634a22cbd0878cba2afb744a92a2543
Sprunk <92063646+MaxAdams0@users.noreply.github.com>
2023-03-06 21:31:56 -0800

    v0.9 Release

--- translate.py
+++ translate.py
@@ -4,69 +4,109 @@ import os
 import torch
 import pyaudio
 import wave
-from pynput.keyboard import Key, Listener
-from datetime import datetime
+import keyboard as kb
+import time
+import voicevox as vvx
+import asyncio
+import winsound
 
 # PyAudio stream variables
 chunk = 2048  # Record in chunks of 2048 samples
-sample_format = pyaudio.paInt32  # 32 bits per sample
+sample_format = pyaudio.paInt16  # 32 bits per sample
 channels = 2
 fs = 44100  # Record at 44100 samples per second
-seconds = 3
-filename = "tmp_voice_file.wav"
+temp_filename = "tmp_rec_file.wav"
 
 pyaudioIF = pyaudio.PyAudio() # Create PortAudio Interface
 
-voice_stream = pyaudioIF.open(format=sample_format,
+def func_timer(func):
+    def wrapper():
+        start = time.time()
+        func()
+        end = time.time()
+        print(f"Function '{func.__name__}' took {(end-start)*1000}ms to complete.")
+    return wrapper
+
+def init_recorder():
+    global voice_stream
+    global audio_frames
+
+    voice_stream = pyaudioIF.open(format=sample_format,
                 channels=channels,
                 rate=fs,
                 frames_per_buffer=chunk,
                 input=True)
-audio_frames = []
+    audio_frames = []
 
-def on_key_press(key):
-    if key is not Key.backspace: return
+async def gen_speech(jp_text):
+    async with vvx.Client() as client:
+        audio_query = await client.create_audio_query(
+            jp_text, speaker=8
+        )
+        with open("voice.wav", "wb") as f:
+            f.write(await audio_query.synthesis())
+
+def record_voice():
     audio_frames.append(voice_stream.read(chunk))
+    print(f"Frame count: {len(audio_frames)}")
 
-def on_key_release(key):
+@func_timer
+def process_voice():
     # Stop recording
     voice_stream.stop_stream()
+    voice_stream.close()
 
     # Open and save recording to .wave file for whisper
-    wave_file = wave.open(filename, 'wb')
+    wave_file = wave.open(temp_filename, 'wb')
     wave_file.setnchannels(channels)
     wave_file.setsampwidth(pyaudioIF.get_sample_size(sample_format))
     wave_file.setframerate(fs)
     wave_file.writeframes(b''.join(audio_frames))
     wave_file.close()
-    clock_start = datetime.now()
     
-    if key is not Key.backspace: return
     # Make sure to run on GPU if avalible
     device = "cuda" if torch.cuda.is_available() else "cpu"
     # Get the path of audio file (DEBUG)
-    audio_path = os.path.join(os.path.dirname(__file__), filename)
+    audio_path = os.path.join(os.path.dirname(__file__), temp_filename)
     # Use Whisper (OPENAI) to transcribe from audiofile
     text_model = Whisper.load_model("base").to(device)
     whisper_result = text_model.transcribe(audio_path, language="en", fp16=False)
     # The 'transcribe' function has much more info, we just need text for translation
-    whisper_text = whisper_result["text"]
-    print(f"English:  {whisper_text}")
+    eng_text = whisper_result["text"]
+    print(f"English:  {eng_text}")
 
-    # use MyMemory as a translator. (source=, target=)
-    translate_output = Translator("english","japanese").translate(whisper_text)
-    print(f"Japanese: {translate_output}")
-    direct_translate_output = Translator("japanese","english").translate(translate_output)
-    print(f"Direct  : {direct_translate_output}")
+    # Counts and limits punctuations. VoiceVox does not allow over 7. (NOT YET)
+    # Converts eng -> jp for VoiceVox; Converts eng -> jp -> eng for understanding what
+    # the translator came up with.
+    jp_text = Translator("english","japanese").translate(eng_text)
+    print(f"Japanese: {jp_text}")
+    direct_jp_text = Translator("japanese","english").translate(jp_text)
+    print(f"Direct  : {direct_jp_text}")
 
-    # See how long the translation took
-    clock_end = datetime.now()
-    clock_delta = clock_end - clock_start
-    clock_delta = round(clock_delta.total_seconds() * 1000, 3)
-    print(f"ProcTime: {(clock_delta)}ms")
+    # Generate speech .wave file using VoiceVox
+    try:
+        asyncio.run(gen_speech(jp_text))
+    except:
+        print("Speech processing failed.")
+    init_recorder()
 
-while True:
-    with Listener(
-        on_press=on_key_press,
-        on_release=on_key_release) as listener:
-        listener.join()
+def main():
+    print("Program Start")
+    init_recorder()
+    print("Finished PyAudio initialization.")
+    while True:
+        # Event = any key press; KEY_DOWN = press; KEY_UP = release
+        event = kb.read_event()
+        if event.event_type == kb.KEY_DOWN and event.name == 'backspace':
+            record_voice()
+        if event.event_type == kb.KEY_UP and event.name == 'backspace':
+            print("Beginning processing.")
+            process_voice()
+            print("Finished processing.")
+            try:
+                winsound.PlaySound(vvx_filename, winsound.SND_FILENAME)
+            except:
+                print("Speech playback failed.")
+
+if __name__ == "__main__":
+    main()
